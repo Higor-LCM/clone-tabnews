054514ec6751e547916b8c8dd652d503e819112e
Higor Cardoso <88698061+Higor-LCM@users.noreply.github.com>
2024-01-05 20:16:21 +0000

    adciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function home() {
-    return <h1> Mano que curso phoda!! Depois você assiste a aula 7 completa, a forma como ele explica a comunicação do servidor com o cliente é sensacional!!</h1>
+  return (
+    <h1>
+      {" "}
+      Mano que curso phoda!! Depois você assiste a aula 7 completa, a forma como
+      ele explica a comunicação do servidor com o cliente é sensacional!!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default home;
